4fd4d80d3e604a1654a0b6ca18f10368949fec7f
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-08-31 01:51:43 +0300

    Update andmeta_lisamine.sql

--- andmeta_lisamine.sql
+++ andmeta_lisamine.sql
@@ -1,34 +1,57 @@
-INSERT INTO guest(id,first_name, Last_name, member_since)
-Values(1,'Deniss','Gorjunov','2022-03-11')
+insert into guest(first_name,last_name, member_since)
+values('Deniss','Gorjunov', '2023-03-21')
 
-INSERT INTO guest(id,first_name, Last_name, member_since)
-Values(2,'Timur','Baroda','2022-03-11')
-INSERT INTO guest(id,first_name, Last_name, member_since)
-Values(3,'Sasha','Baroda','2022-03-11')
-INSERT INTO guest(id,first_name, Last_name, member_since)
-Values(4,'Katja','Baroda','2022-03-11')
-INSERT INTO guest(id,first_name, Last_name, member_since)
-Values(5,'Lvonok','Baroda','2022-03-11')
+insert into guest(first_name,last_name, member_since)
+values('Oleg','Voroshenko', '2023-03-21')
 
-Insert into room(id,number,name_, status_, smoke)
-values(1,'232','Voughter', 'Maksad', 1);
+insert into guest(first_name,last_name, member_since)
+values('Sasha','Petrenko', '2023-03-21')
 
+insert into guest(first_name,last_name, member_since)
+values('Luki','Kapysta', '2023-03-21')
 
-Insert into room(id,number,name_, status_, smoke)
-values(2,'442','Loughter', 'Maksad', 1);
+insert into guest(first_name,last_name, member_since)
+values('Roman','Sili', '2021-03-21')
 
-Insert into room(id,number,name_, status_, smoke)
-values(3,'111','Vikki', 'ei maksa', 0);
-Insert into room(id,number,name_, status_, smoke)
-values(4,'121','Lopo', 'ei maksa', 0);
-Insert into room(id,number,name_, status_, smoke)
-values(5,'321','Lkpo', 'Maksad', 1);
+insert into room_type(description_, max_capacity)
+values('Lux', 4)
 
-Insert into reservation(id, date_in, date_out, made_by)
-values(1, '2023-04-13','2023-04-23', 'Aleksander')
-Insert into reservation(id, date_in, date_out, made_by)
-values(2, '2022-04-13','2022-04-23', 'Roma')
-Insert into reservation(id, date_in, date_out, made_by)
-values(3, '2022-01-13','2022-01-23', 'Ksenia')
-Insert into reservation(id, date_in, date_out, made_by)
-values(4, '2002-01-13','2002-01-23', 'Timur')
+insert into room_type(description_, max_capacity)
+values('Cheap', 1)
+
+insert into reservation(guest_id,date_in,date_out,made_by)
+values(1,'2023-03-23','2023-03-30','Hotel');
+
+insert into reservation(guest_id,date_in,date_out,made_by)
+values(2,'2023-01-23','2023-01-30','Hotel');
+
+insert into reservation(guest_id,date_in,date_out,made_by)
+values(3,'2023-05-23','2023-05-30','Hotel');
+
+insert into reservation(guest_id,date_in,date_out,made_by)
+values(4,'2023-06-23','2023-06-30','Hotel');
+
+insert into reservation(guest_id,date_in,date_out,made_by)
+values(5,'2023-06-23','2023-06-30','Hotel');
+
+Insert into reserved_room(
+number_of_rooms, room_type_id, reservation_id, status_)
+values(1,1,1,'Closed')
+
+Insert into reserved_room(
+number_of_rooms, room_type_id, reservation_id, status_)
+values(2,2,2,'Closed')
+
+Insert into reserved_room(
+number_of_rooms, room_type_id, reservation_id, status_)
+values(6,6,6,'Closed')
+
+Insert into reserved_room(
+number_of_rooms, room_type_id, reservation_id, status_)
+values(3,3,3,'Closed')
+
+INSERT INTO occupied_room (check_in,check_out, room_id, reservation_id)
+VALUES ('2021-07-07 10:05:23.187', '2022-08-07 10:05:23.187', 5, 6);
+
+insert into hosted_at(guest_id, occupied_room_id)
+values(5,15)
